5032595ec7da3efd5f3f3304b9d8d3ead5d61390
Fahedoo <fahedismaili32@gmail.com>
2025-09-18 22:11:01 +0200

    Paths testing because the bg doesnt load

--- src/data/projets.js
+++ src/data/projets.js
@@ -9,7 +9,7 @@ const projets = [
       "Indispensable pour un développeur, ce portfolio est une interface professionnelle accessible à tous permettant de mettre en valeur mes compétences et mon expérience, me présenter pour qu'on en apprenne plus sur moi mais aussi de me contacter si intéressé !",
       "Ce projet m'a permis de mettre en pratique mes compétences en React, en gestion de routing, et en design responsive.",
     ],
-    miniature: "../../projets/portfolio/pf_tn.png",
+    miniature: "public/projets/portfolio/pf_tn.png",
     images: [
       "../../projets/portfolio/pf_1.png",
       "../../projets/portfolio/pf_2.png",
