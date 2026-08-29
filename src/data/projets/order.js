9c38a0bd90732b1002a5314d4aeafc59e1fb9be7
Fahedoo <fahedismaili32@gmail.com>
2026-08-29 19:06:11 +0200

    Made a file to manage projects order

---
+++ src/data/projets/order.js
@@ -0,0 +1,11 @@
+/**
+ * Ordre d'affichage des projets (du plus récent / prioritaire en haut).
+ * Pour réordonner : déplace simplement les lignes.
+ * Les slugs doivent correspondre aux fichiers dans entries/ et à la propriété slug.
+ */
+export default [
+  "niltiplication",
+  "nightofterror",
+  "portfolio",
+  "manga-presentation",
+];
